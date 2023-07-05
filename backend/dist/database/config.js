fa44380b0620f8f34d6a6b6a5409014760fe3f26
Chiranjeeb Panda <chiranjeebpanda01@gmail.com>
2023-07-05 19:51:58 +0530

    add connection string to a config.ts file

---
+++ backend/dist/database/config.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.connection_string = void 0;
+exports.connection_string = "mongodb+srv://iamchiranjeebpanda:WtteayAK9BSyb2vc@cluster0.t0x8iin.mongodb.net/blood_banks_india?ssl=true&sslValidate=true";
