c3a2104abbc936120de3ff735bf1734f7e0be8c6
Chiranjeeb Panda <chiranjeebpanda01@gmail.com>
2023-07-19 16:45:55 +0530

    add and configure  middlewares- bodyParser and CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
 import { connectToDatabase } from "./database/establishConnection";
 
 const app = express();
 
+//* Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+//* Server Port Definition
 connectToDatabase().then(() => {
     app.listen(8000);
 })
